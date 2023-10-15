20e4869935bdb79b529f1d75791254664a447ab5
sikandarmir <sikandarmir4@yahoo.com>
2023-10-15 03:45:33 -0700

    new_againa

--- app.py
+++ app.py
@@ -17,32 +17,37 @@ chat_data = {}
 
 @app.route('/chat', methods=['POST'])
 def chat():
-    data = request.json
-    user_id_str = data.get('user_id')
-    message = data.get('message')
-
-    print(user_id_str)
-
-    chat_id_str=1
-
-    # Check if user_id and chat_id can be converted to integers
     try:
-        user_id = int(user_id_str)
-        chat_id = int(chat_id_str)
-    except ValueError:
-        return jsonify({'error': 'user_id and chat_id must be valid integers'}), 400
+        data = request.json
+        user_id_str = data.get('user_id')
+        message = data.get('message')
+        print(message)
+
+        print(user_id_str)
+
+        chat_id_str=1
+
+        # Check if user_id and chat_id can be converted to integers
+        try:
+            user_id = int(user_id_str)
+            chat_id = int(chat_id_str)
+        except ValueError:
+            return jsonify({'error': 'user_id and chat_id must be valid integers'}), 400
 
 
 
-    # Message you want to use for generating a response
+        # Message you want to use for generating a response
 
 
-    # Call the function
-    result=integration_db(user_id, chat_id,message)
+        # Call the function
+        result=integration_db(user_id, chat_id,message)
 
 
-    # Return a response with just the message
-    return jsonify({'message': result})
+        # Return a response with just the message
+        return jsonify({'message': result})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
